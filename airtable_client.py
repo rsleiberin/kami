ddd40b9023312cac4fd8b8e8823e6d3c40f09dea
rsleiberin <rsleiberin@gmail.com>
2023-10-08 17:54:55 -0700

    Added get_methods() method to AirtableClient and explore socket nesting

--- airtable_client.py
+++ airtable_client.py
@@ -1,4 +1,5 @@
 from pyairtable import Api
+import inspect
 import config
 import logging
 
@@ -31,19 +32,32 @@ static_table_dictionary = {
 
 class AirtableClient:
     def __init__(self,api_key):
+        """Initializes AirtableClient with API key."""
         self.api = Api(api_key)
         self.base_dictionary ={'Kami':config.AIRTABLE_BASE_ID}
         self.active_baseID = self.base_dictionary['Kami']
         self.table_dictionary = {}
     
+    def get_methods(self):
+        """Returns dictionary of method names, docstrings, and argument signatures."""
+        try:
+            methods = {
+            func: {
+                'args': inspect.signature(getattr(self, func)).parameters.keys(),
+                'doc': getattr(self, func).__doc__.split('\n')[0] if getattr(self, func).__doc__ else ''
+            } 
+            for func in dir(self) 
+            if callable(getattr(self, func)) and not func.startswith("__")
+            }
+            return methods
+        except Exception as e:
+            print(e)
+            logging.error(str(e))
+        
+    #READ
     #retrieve base dictionary
     def get_bases(self):
-        '''
-        Not designed yet
-        Fetches all bases from workspace and updates self.base_dictionary
-        Returns:
-            dict_keys: A dict_keys object of all base names from the self.base_dictionary
-        '''
+        """Fetches all bases, updates self.base_dictionary, returns dict_keys of base names."""
         try:    
             return self.base_dictionary.keys()
         except Exception as e:
@@ -53,12 +67,7 @@ class AirtableClient:
 
     #retrieve table dictionary
     def get_tables(self):
-        '''
-        Currently just grabs the static_table_dictionary, later will upgrade
-        Fetches all tables from base and updates self.table_dictionary
-        Returns:
-            dict_keys: A dict_keys object of all table names from self.table_dictionary
-        '''
+        """Fetches all tables, updates self.table_dictionary, returns dict_keys of table names."""
         try:
             self.table_dictionary = static_table_dictionary
             return self.table_dictionary.keys()
@@ -68,15 +77,7 @@ class AirtableClient:
 
     #retrieve records using table name
     def get_records(self, table_name):
-        """
-        Fetches all records from the specified table.
-        
-        Args:
-            table_name (str): The name of the table to fetch records from.
-            
-        Returns:
-            list: A list of dictionary records from the specified table.
-        """
+        """Fetches all records from specified table_name, returns list of dictionary records."""
         try:
             # Access a table by name from the baseID and tableID
             table = self.api.table(self.active_baseID, self.table_dictionary.get(table_name))
@@ -85,6 +86,27 @@ class AirtableClient:
             return records
         except Exception as e:
             logging.error(str(e))
+'''
+to-do
+def get_record_by_id(self, table_name, record_id):
+    # implementation...
+def get_records_with_filter(self, table_name, filter_expression):
+    # implementation...
+def get_records_sorted(self, table_name, sort_field, ascending=True):
+    # implementation...
+def get_records_paginated(self, table_name, page_size, page_number):
+    # implementation...
+def get_records_by_date_range(self, table_name, start_date, end_date):
+    # implementation...
+def get_records_with_fields(self, table_name, fields_list):
+    # implementation...
+def search_records(self, table_name, query):
+    # implementation...
+
+'''
+if __name__ == "__main__":
+    instance = AirtableClient('1')
+    print (instance.get_methods())
 
 '''
 if __name__ == "__main__":
@@ -116,4 +138,8 @@ if __name__ == "__main__":
 dict_keys(['Kami'])
 dict_keys(['AgentMessages', 'Tables', 'Keywords', 'Roles', 'UserMessages', 'Sockets', 'Contexts', 'AuditLogs', 'Changes', 'Responses', 'ResponseKeywords', 'Models', 'AgentSockets', 'Agents', 'Sessions', 'Users'])
 [{'id': 'recBmcZd4r3D1bFPr', 'createdTime': '2023-10-06T01:35:31.000Z', 'fields': {'RoleDescription': "The Assistant role represents messages that store previous assistant responses or can be used to provide examples of desired behavior. These messages help shape the conversation's context and guide the assistant's behavior.\n", 'RoleName': '"assistant"', 'Note': '"Assistant messages serve multiple purposes in a conversation. They can store previous responses for reference, showcase the assistant\'s capabilities, or demonstrate the desired behavior to the model. While not as common as System and User messages, they play a valuable role in shaping the interaction."\n', 'Models': ['recVVavZLrEBY4Yuh', 'rec2bcgGvrFuBwwlc', 'recq1CHxez5td41kY', 'recjOJxH6e1Iafqii', 'receqQIGClmc02RjU'], 'RoleID': 3}}, {'id': 'recGoWNTz1R312vfK', 'createdTime': '2023-10-06T01:33:21.000Z', 'fields': {'RoleDescription': "The System role represents messages that set the behavior and instructions for the assistant within a conversation. System messages help guide the assistant's responses and actions.\n", 'RoleName': '"system"', 'Note': ' "The System role is typically used at the beginning of a conversation to provide high-level instructions or to modify the personality and behavior of the assistant. It helps shape the context for the conversation and influences how the assistant responds to subsequent user messages. While optional, a well-crafted System message can significantly impact the conversation\'s outcome."\n', 'Models': ['recVVavZLrEBY4Yuh', 'rec2bcgGvrFuBwwlc', 'recq1CHxez5td41kY', 'recjOJxH6e1Iafqii', 'receqQIGClmc02RjU'], 'Sockets': ['recXkUNJHo3ELhsZv', 'recwMjhUYZvhsZtAF', 'receFtpM0OLlL5UGf', 'recvW56wZ7PAvW2Xk', 'recEgeEU130ZA8a5X', 'rech3iip1yynHKPH1', 'recQS2o2U3jiKWvl9'], 'RoleID': 1}}, {'id': 'recNPDi6R1ZKKNoEp', 'createdTime': '2023-10-06T01:35:22.000Z', 'fields': {'RoleDescription': 'The User role represents messages from the user, including requests, queries, or comments. User messages drive the conversation by providing input for the assistant to respond to.\n', 'RoleName': '"user"', 'Note': '"User messages are an essential part of the conversation as they define the user\'s intent and the tasks they want the assistant to perform. These messages typically come after the System message and provide context and instructions for the assistant to generate relevant responses."\n', 'Models': ['recVVavZLrEBY4Yuh', 'rec2bcgGvrFuBwwlc', 'recq1CHxez5td41kY', 'recjOJxH6e1Iafqii', 'receqQIGClmc02RjU'], 'RoleID': 2}}]
+'''
+
+'''
+
 '''
