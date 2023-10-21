b6e8b1d240a7d9681875717786f29eb461dfe0a5
rsleiberin <rsleiberin@gmail.com>
2023-10-20 20:45:57 -0700

    creating standards directory and beginning the modularization of the safety directory before integration with chatGPT.

--- llm/chat_gpt_llm/chat_gpt_functions.py
+++ llm/chat_gpt_llm/chat_gpt_functions.py
@@ -1,27 +1,46 @@
-# chat_gpt_functions.py
-
+# Import Section: Standard libraries, third-party libraries, application libraries
 import json
+from utils.utils import print_tracer, handle_error  # Assuming print_tracer and handle_error are in utils.utils
 
 class ChatGPTAssistant:  # Extend the existing class
+
     def handle_function_call(self, function_name, function_args):
-        # Implement safety checks, logging, etc. here
-        if self.is_safe_function(function_name, function_args):
-            json_string = json.dumps({
-                "function_name": function_name,
-                "arguments": function_args
-            })
-            return Run(json_string)  # Run function imported from run_function.py
-        else:
-            # Handle unsafe function calls
-            return "Unsafe function call attempted."
+        """
+        Handle function calls received from ChatGPT.
+        """
+        print_tracer("ChatGPTAssistant", "handle_function_call", "Start")
+        
+        try:
+            # Implement safety checks
+            if self.is_safe_function(function_name, function_args):
+                print_tracer("ChatGPTAssistant", "handle_function_call", "Safe Function")
+                
+                json_string = json.dumps({
+                    "function_name": function_name,
+                    "arguments": function_args
+                })
+                
+                return Run(json_string)  # Run function imported from run_function.py
+            else:
+                print_tracer("ChatGPTAssistant", "handle_function_call", "Unsafe Function")
+                
+                # Handle unsafe function calls
+                return "Unsafe function call attempted."
+        except Exception as e:
+            print_tracer("ChatGPTAssistant", "handle_function_call", "Error", str(e))
+            handle_error("CH001")  # Hypothetical error code
+        finally:
+            print_tracer("ChatGPTAssistant", "handle_function_call", "End")
 
     def is_safe_function(self, function_name, function_args):
+        """
+        Check if the function call is safe to execute.
+        """
+        print_tracer("ChatGPTAssistant", "is_safe_function", "Start")
+        
         # Implement your safety checks here
-        return function_name in approved_functions_list  # This list needs to be defined
+        result = function_name in approved_functions_list  # This list needs to be defined
 
-    def update_function_registry(self):
-        # Update 'available_functions' based on a JSON file or a database query
-        pass
-
-    def query_available_functions(self):
-        return json.dumps({"available_functions": list(available_functions.keys())})  # available_functions needs to be imported or defined
+        print_tracer("ChatGPTAssistant", "is_safe_function", "End")
+        
+        return result
